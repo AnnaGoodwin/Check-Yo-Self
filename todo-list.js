25bcefc9118f174207479515e91f75f6f3fb2503
Anna Goodwin <goodwin.musician@gmail.com>
2019-04-12 17:26:49 -0600

    Initial commit

---
+++ todo-list.js
@@ -0,0 +1,21 @@
+class ToDoList {
+    constructor(id) {
+        this.id = id,
+        this.title = '' ,
+        this.tasks = true || false,
+        this.urgent = []
+
+    }
+    saveToStorage {
+
+    }
+    deleteFromStorage {
+
+    }
+    updateToDo {
+
+    }
+    updateTask {
+
+    }
+}
